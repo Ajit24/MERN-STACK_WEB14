4f1dc50ca41b99dbc7936fdd6c79ff31160d1b0c
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-03-07 09:25:57 +0530

    c1

---
+++ unit-5/sprint-1/evaluation/c-2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       <h3>AAAAAA</h3>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
