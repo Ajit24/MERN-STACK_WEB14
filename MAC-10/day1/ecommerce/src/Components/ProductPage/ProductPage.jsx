19ca34fcb8bcf2a3e70d9f6a3b2f60edbd87b174
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-05-12 19:00:16 +0530

    day4 cart page

--- MAC-10/day1/ecommerce/src/Components/ProductPage/ProductPage.jsx
+++ MAC-10/day1/ecommerce/src/Components/ProductPage/ProductPage.jsx
@@ -1,56 +1,71 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 import "./ProductPage.css";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const ProductPage = () => {
-  const [data,setData]=useState([])
+  const [data, setData] = useState([]);
   const navigate = useNavigate();
-  useEffect(() =>{
+
+  useEffect(() => {
     loadData();
-  },[])
-  const loadData =async () =>{
-    try{
-    const res =  axios({
-      method: 'GET',
-      url:"http://localhost:3000/products",}).then((res)=>{
-      setData(res.data)
-    })
-    console.log(res)
-    // setData(d);
-    }catch(e){
-      console.log("error",e);
+  }, []);
+
+  const loadData = async () => {
+    try {
+      const res = axios({
+        method: "GET",
+        url: "http://localhost:3000/products",
+      }).then((res) => {
+        setData(res.data);
+      });
+      console.log(res);
+      // setData(d);
+    } catch (e) {
+      console.log("error", e);
     }
-  }
+  };
+
   //    sort by price
   const handleSort = (e) => {
     // dispach handle sort action to the store
-
   };
+
   return (
     <>
+      <div className="mainIndiv">
       <h2>Products For Shop</h2>
       <select onChange={handleSort}>
         <option>--sort by --</option>
         <option value="asc">low to high</option>
         <option value="desc">high to low</option>
       </select>
+
+
       <div className="products-list">
+
 
         {/* map throught th products  list and display the results */}
         {data &&
-          data.map((item,i) => {
-            return <div onClick={()=>navigate(`/SingleProduct/${item.id}`)} key={i}>
-            {/* <Link to={`/products/${item.id}`}> */}
-            <img src={`${item.image}`}/>
-            <h3>{item.title}</h3>
-            <p>{item.brand}</p>
-            <p>Price-{item.price}</p>
-            {/* </Link> */}
-            
-            </div>;
+          data.map((item, i) => {
+            return (
+              <div className="Individual"
+                onClick={() => navigate(`/SingleProduct/${item.id}`)}
+                key={i}
+              >
+                {/* <Link to={`/products/${item.id}`}> */}
+                <div><img src={`${item.image}`} /></div>
+                <div><h3>{item.title}</h3></div>
+                <div><p>{item.brand}</p></div>
+                <div><p>Price-{item.price}</p></div>
+                {/* </Link> */}
+              </div>
+            );
           })}
+
+          
+      </div>
       </div>
     </>
   );
--- MAC-10/day1/ecommerce/src/Components/SingleProduct/SingleProduct.jsx
+++ MAC-10/day1/ecommerce/src/Components/SingleProduct/SingleProduct.jsx
@@ -1,31 +1,44 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./SingleProduct.css";
 
 export const SingleProductList = () => {
-  const {id}  = useParams();
+  
+  const { id } = useParams();
   const [data, setData] = useState(null);
- console.log("data",id);
+  console.log("data", id);
+
   useEffect(() => {
     axios({
       method: "get",
-      url: `http://localhost:3000/products/${id}`
+      url: `http://localhost:3000/products/${id}`,
     })
       .then((res) => setData(res.data))
       .catch((err) => console.log(err));
-      console.log(data)
+    console.log(data);
   }, [id]);
 
+  const handleAddCart =() => {
+    console.log("cart")
+  }
   // make a request to get the details
   return (
     <>
-         <h1>SinglePage</h1>
-         {data && <> <h2>{data.title}</h2>
-               <img src= {data.image}/>
-         
-               <h3> {data.brand} </h3>
-               <h3> {data.price} </h3>
-         </>}
+      <div className="SingleProductList">
+        {/* <h1>SinglePage</h1> */}
+        {data && (
+          <>
+            {" "}
+            
+            <div className="ImageProduct"><img src={data.image} /></div>
+            <div><h2>{data.title}</h2></div>
+            <div><h3> {data.brand} </h3></div>
+            <div><h3> Rs. {data.price} </h3></div>
+            <div><button onClick={handleAddCart}>Add To Cart</button></div>
+          </>
+        )}
+      </div>
     </>
-    )
-  }
+  );
+};
